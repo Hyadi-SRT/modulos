090250578e453653268903ec83098ef932a3831e
JFT-314 <j_ft@live.com.mx>
2021-09-10 17:38:08 -0500

    Modificacion constructor Drag ventilacion

--- variables/DRAG_ventilacion.py
+++ variables/DRAG_ventilacion.py
@@ -1,6 +1,9 @@
 class drag_ventilacion:
-    def __init__(self, dinamic_pressure, drag_area):
+    def __init__(self, dinamic_pressure, drag_area, drag):
         self.dinamic_pressure = dinamic_pressure
         self.drag_area = drag_area
+        self.drag = drag
 
-        
+    def drag_coefficient(self):
+        self.drag/(self.dinamic_pressure * drag_area)
+
