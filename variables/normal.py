58903ac40639f5c22653c52e9e8319fe6113304c
JFT-314 <j_ft@live.com.mx>
2021-09-11 02:23:32 -0500

    Consideraciones adicionales en Clase Normal

--- variables/normal.py
+++ variables/normal.py
@@ -6,7 +6,13 @@ class Normal:
 		self.angle = angle
 
 	def calcula_fuerza_pendiente(self):
-		return self.force * math.cos(math.radians(self.angle));
+		if self.angle > 0:
+			return -self.force * math.cos(math.radians(self.angle))
+		else:
+			if self.angle < 0:
+				return self.force * math.cos(math.radians(self.angle))
+			else:
+				return 0
 
-time = Normal(24, 5.1)
+time = Normal(24, 0)
 print(str(time.calcula_fuerza_pendiente()))
