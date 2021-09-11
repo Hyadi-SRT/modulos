15c9e5bbbc3570f74290713e1be0ef320659ce6d
JFT-314 <j_ft@live.com.mx>
2021-09-10 19:03:51 -0500

    Modificacion Clase Normal y creacion de funcion push_force en clase Normal

--- variables/normal.py
+++ variables/normal.py
@@ -1,11 +1,27 @@
-class Normal:
-    def __init__(self, valor) -> None:
-        self._valor = valor
-    
-    @property
-    def valor(self):
-        return self._valor
+import math
 
-    @valor.setter
-    def valor():
-        pass
+class Normal:
+    force = 0
+    angle = 0
+
+    def __init__(self, f, a):
+        self.force = f
+        self.angle = a
+
+ 
+    def display(self):
+        print(str(self.force)+ " " + str(self.angle))
+
+    @classmethod
+    def push_force(self):
+        return self.force * math.cos(self.aGrados(self.angle))
+
+    @classmethod
+    def aGrados(self, radianes):
+        return (math.pi * radianes) / 180
+
+
+norm = Normal(250, 50)
+print (norm.display())
+print(norm.aGrados(50))
+print(norm.push_force())
