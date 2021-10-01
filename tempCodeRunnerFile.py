c6bcb2dd7a8342e4d146eb18109133e01d262afe
hum-sc <humbertoantsc@gmail.com>
2021-09-30 21:03:25 -0500

    DynamicPressure y Drag reescritos"

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-elf._total = 0
-        self._coefficient = 0
--- variables.py
+++ variables.py
@@ -49,11 +49,11 @@ class RollingResistance:
         # Calcula el coeficiente de Rolling Resistance y lo coloca
         # Recibe una sola tupla con 3 valores
         # -staticForce
-        # -momentValentForce
-        # -speed
+        # -moment_valent_force
+        # -velocity
 
-        staticForce, momentValentForce, speed = args
-        self._coefficient = staticForce + (momentValentForce*speed)
+        staticForce, moment_valent_force, velocity = args
+        self._coefficient = staticForce + (moment_valent_force*velocity)
 
     @property
     def total(self) -> float:
@@ -66,7 +66,58 @@ class RollingResistance:
         # Calcula el valor del Rolling Resistance total
         # Recibe un argumento (el valor de la normal)
         self._total = self.coefficient * normal
+
+class DynamicPressure:
+    def __init__(self) -> None:
+        self._density = 0
+        self._relative_velocity = 0
+        self._value = 0
     
+    @property
+    def value(self) -> float:
+        return (self._density * self._relative_velocity**2)/2
+    
+    @property
+    def density(self):
+        return self._density
+    
+    @density.setter
+    def density(self, value):
+        self._density = value
+    
+    @property
+    def relative_velocity(self):
+        return self._relative_velocity
+
+    @relative_velocity.setter
+    def relative_velocity(self, value):
+        self._relative_velocity = value
+    
+class DragForce:
+    def __init__(self, dp:DynamicPressure) -> None:
+        self._coefficient = 0
+        self._area = 0
+        self._dynamic_pressure = dp.value
+    
+    @property
+    def value(self):
+        return self._coefficient*self._area*self._dynamic_pressure
+    
+    @property
+    def coefficient(self):
+        return self._coefficient
+    @coefficient.setter
+    def coefficient(self, value):
+        self._coefficient = value
+
+    @property
+    def area(self):
+        return self._area
+    @area.setter
+    def area(self, value):
+        self._area = value
+
+
 if __name__ == '__main__':
     normal = Normal()
     normal.angle=5.71
@@ -81,3 +132,14 @@ if __name__ == '__main__':
     print(f"Rolling coefficient = {rollin.coefficient}")
     print(f"Rolling resistance = {rollin.total}")
     print(f"Normal = {normal.value}")
+
+    dp = DynamicPressure()
+    dp.density=1.25
+    dp.relative_velocity=15
+
+    drag = DragForce(dp)
+
+    drag.area=0.11
+    drag.coefficient=1
+
+    print(drag.value)
